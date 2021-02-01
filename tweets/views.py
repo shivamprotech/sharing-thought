1b1cb900650ca1e0ae20d241bf8f8d2589288518
shivamprotech <shivamkhandelwal555@gmail.com>
2021-02-01 17:54:55 +0530

    Add Permission and Authentication decorator

--- tweets/views.py
+++ tweets/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from tweets.forms import TweetForm
-from tweets.models import Tweet
-from tweets.serializer import TweetSerializer
-
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND
 )
+
+from tweets.forms import TweetForm
+from tweets.models import Tweet
+from tweets.serializer import TweetSerializer
 
 # Create your views here.
 
@@ -22,6 +23,7 @@ def home_view(request):
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
